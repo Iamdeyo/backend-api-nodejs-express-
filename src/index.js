0f9cd7da6c82d44a56694f17d00daadaa4e2ddcb
Ademola Taiwo <102713835+Iamdeyo@users.noreply.github.com>
2024-01-05 14:15:05 +0100

    feat: middlewares and env variables implemented

--- src/index.js
+++ src/index.js
@@ -1,9 +1,28 @@
 import express from 'express';
+import dotenv from 'dotenv';
+import morgan from 'morgan';
+import { xss } from 'express-xss-sanitizer';
+import helmet from 'helmet';
+import cors from 'cors';
 import router from './routes/index.js';
 
 const app = express();
+dotenv.config();
 
-const port = 3000;
+const port = process.env.PORT || 3000;
+
+/**
+ *  Application Level Middleware
+ */
+app.use(morgan('dev'));
+app.use(
+  cors({
+    origin: '*',
+  }),
+);
+app.use(xss());
+app.use(helmet());
+app.use(express.json());
 
 app.use(router);
 app.get('/', (req, res) => {
